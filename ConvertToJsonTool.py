b2d2c11200d199cde60a6d769151968e9a976840
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2021-10-09 19:58:00 +0900

    エクセルの読み込み。

--- ConvertToJsonTool.py
+++ ConvertToJsonTool.py
@@ -1,4 +1,51 @@
+#ウィンドウを表示するためのライブラリ
 import tkinter as tk
+
+#Excelファイルを開くためのライブラリ
+#xlsxファイルには対応しなくなった
+import xlrd
+
+import openpyxl
+
+
+#エクセルの要素を読み込む
+def lead_sheet(sheet):
+    lines = []
+    for line in sheet:
+        #1行ずつ読み込んで、リストに追加する。
+        lines.append(lead_col(line))
+    return lines
+
+#1行ごとの列を読み込む。
+def lead_col(line):
+    cells = []
+    #1列ずつ読み込む。
+    for cell in line:
+        cells.append(cell.value)
+    return cells
+
+#Excelのファイルを読み込む。
+book = openpyxl.load_workbook("file/data.xlsx")
+#シート枚数を取得。
+sheet_number = len(book.worksheets)
+for number in range(sheet_number):
+    #number番目のシートを取得。
+    sheet = book.worksheets[number]
+    lines = []
+    #1行ずつ読み込む。
+    for row in sheet.rows:
+        line = []
+        #1列ずつ読み込む。
+        for cell in row:
+            line.append(cell.value)
+        #1行ずつリストに追加する。
+        #lines.append(line)
+    lines = lead_sheet(sheet)
+    print(sheet)
+    print(lines)
+
+
+
 
 
 class Application(tk.Frame):
@@ -18,3 +65,6 @@ class Application(tk.Frame):
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
+
+
+
