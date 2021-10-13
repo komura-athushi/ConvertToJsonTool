cdc8c1580ad003ae490dea1b671296478a80d8a7
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2021-10-13 11:50:44 +0900

    リストボックスが上手く表示できません。

--- convert_to_json_tool.py
+++ convert_to_json_tool.py
@@ -1,12 +1,17 @@
 #ウィンドウを表示するためのライブラリ
 import tkinter as tk
-
+import tkinter.ttk as ttk
+from tkinter import filedialog
+from tkinter import messagebox
 #Excelファイルを開くためのライブラリ
 #xlsxファイルには対応しなくなった
 #import xlrd
 
 #エクセルのデータを扱う。
 import excel_data
+
+#定数データ
+import constant
 
 #エクセルを読み込む。
 data = excel_data.ExcelData()
@@ -20,18 +25,37 @@ class Application(tk.Frame):
         self.master = master
         #名前を設定
         self.master.title('Convert To Json Tool')
+        window_size = str(constant.WINDOWS_WIDTH) + 'x' + str(constant.WINDOWS_HEIGHT)
         #とりあえずウィンドウはこれくらいで
-        self.master.geometry("600x600")
+        self.master.geometry(window_size)
         #これをしないとフレームがどうのこうので
         #placeしても表示されない
         self.pack(expand=1, fill=tk.BOTH)
-        #メニューの初期化
-        self.init_menu()
+
+        #ボタンの初期化
+        self.init_button()
         #リストの初期化
         self.init_list_box()
 
         #エクエルデータを保持するリスト。
         self.books = []
+
+
+    def init_button(self):
+        #左フレームの作成と設置
+        self.left_frame = ttk.Frame(self.master,width=constant.LEFT_FRAME_WIDTH,height=constant.LEFT_FRAME_HEIGHT)
+        self.left_frame.place(relx=0,rely=0)
+
+        #ボタンの作成
+        self.load_button = tk.Button(self.left_frame, text="Excelファイル読み込み", font=("MSゴシック", "12", "bold"),
+        command=self.load_excel)
+
+        self.export_button = tk.Button(self.left_frame, text="Jsonファイル出力", font=("MSゴシック", "12", "bold"),
+        command=self.export_json)
+
+        #ボタンの設置
+        self.load_button.place(relx=constant.LOAD_BUTTON_RELX,rely=constant.LOAD_BUTTON_RELY)
+        self.export_button.place(relx=constant.EXPORT_BUTTON_RELX,rely=constant.EXPORT_BUTTON_RELY)
 
     def load_excel(self):
         return
@@ -40,18 +64,34 @@ class Application(tk.Frame):
         return
 
     def init_list_box(self):
+
+        #右フレームの作成と設定
+        #self.right_frame = ttk.Frame(self.master,width=constant.RIGHT_FRAME_WIDTH,height=constant.RIGHT_FRAME_HEIGHT)
+        self.right_frame = ttk.Frame(self.master)
+
+        self.project_list = tk.Listbox(self.right_frame,
+        listvariable=None,
+        selectmode='single',
+        width=100,
+        height=200)
+
+        self.right_frame.place(relx=0.5,rely=0)
+
+        #フレームを親にスクロールバーを生成
+        self.project_bar_y = tk.Scrollbar(self.right_frame,orient=tk.VERTICAL,command=self.project_list.yview)
+        #スクロールバーを配置
+        self.project_bar_y.pack(side=tk.RIGHT, fill=tk.Y)
+        #キャンバスとスクロールバーを紐づける
+        self.project_list.config(yscrollcommand=self.project_bar_y.set)
+        #なんかよくわからんがスクロールバーが自動で調整される
+        self.project_list.see('end')
+        
+         #リストを設置
+        self.project_list.pack()
+        
         return
 
-    def init_menu(self):
-        #メニューを生成
-        self.mbar = tk.Menu()
-        #メニューコマンドを生成
-        self.mcom = tk.Menu(self.mbar,tearoff=0)
-        #コマンドを追加
-        self.mcom.add_command(label='Excelファイル読み込み',command=self.load_excel)
-        self.mcom.add_command(label='jsonファイル書き出し',command=self.export_json)
-        self.mbar.add_cascade(label='ファイル',menu=self.mcom)
-        self.master['menu'] = self.mbar
+    
 
 
 #インスタンスを作成。
